55b297d4992d5b6ede7aa30b60a15797e751a745
harelbaruchi <51542052+harelbaruchi@users.noreply.github.com>
2021-05-06 18:05:43 +0300

    Update memberModel.js

--- server/models/memberModel.js
+++ server/models/memberModel.js
@@ -2,6 +2,7 @@ const mongoose    =  require ('mongoose')
 
 let   memberSchema  =  mongoose.Schema
 let   MemberSchema  =  new memberSchema({
+        name:         String,
         id:           String,  
         email:         String,
         city:          String    
